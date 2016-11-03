b3865ce35caac19d58a4630b1eda4b21c50ac3e2
Helena Nevskaya <helena.nevskaya@ya.ru>
2016-11-03 12:11:45 +0300

    load campaigns static file once at start

--- src/campaigns/campaigns.go
+++ src/campaigns/campaigns.go
@@ -108,10 +108,11 @@ func Reload() (err error) {
 		filePath := camp.staticPath + "campaign/" + record.Hash + "/" + record.PageWelcome + ".html"
 		record.Content, err = ioutil.ReadFile(filePath)
 		if err != nil {
-			log.WithField("error", err.Error()).Error("ioutil.ReadFile serve file error")
 			err := fmt.Errorf("ioutil.ReadFile: %s", err.Error())
+			log.WithField("error", err.Error()).Error("ioutil.ReadFile serve file error")
 			return err
 		}
+		log.WithField("path", filePath).Debug("loaded")
 
 		records = append(records, record)
 	}
