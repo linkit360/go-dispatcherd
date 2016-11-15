8cced9b8f710b1904bb8c851154205d7e42cc1f6
Helena Nevskaya <helena.nevskaya@ya.ru>
2016-11-15 15:23:01 +0300

    metrics experiment has been finished

--- src/metrics/metrics.go
+++ src/metrics/metrics.go
@@ -1,59 +1,79 @@
 package metrics
 
 import (
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
 	m "github.com/vostrok/metrics"
 )
 
 var (
-	Overall               prometheus.Counter
-	Access                prometheus.Counter
-	Agree                 prometheus.Counter
-	AgreeSuccess          prometheus.Counter
-	Errors                prometheus.Counter
-	PageNotFoundError     prometheus.Counter
-	IPNotFoundError       prometheus.Counter
-	MsisdnNotFoundError   prometheus.Counter
-	GetInfoByMsisdn       prometheus.Counter
-	NotSupported          prometheus.Counter
-	CampaignHashWrong     prometheus.Counter
-	ContentDeliveryErrors prometheus.Counter
-	ContentdRPCDialError  prometheus.Counter
 	LoadCampaignError     prometheus.Gauge
+	Overall               m.Gauge
+	Access                m.Gauge
+	Agree                 m.Gauge
+	AgreeSuccess          m.Gauge
+	Errors                m.Gauge
+	PageNotFoundError     m.Gauge
+	IPNotFoundError       m.Gauge
+	MsisdnNotFoundError   m.Gauge
+	GetInfoByMsisdn       m.Gauge
+	NotSupported          m.Gauge
+	CampaignHashWrong     m.Gauge
+	ContentDeliveryErrors m.Gauge
+	ContentdRPCDialError  m.Gauge
 )
 
-func newCounterHttpRequests(name, help string) prometheus.Counter {
-	return m.NewCounter("http_requests_"+name, "http_requests "+help)
+func newGaugeHttpRequests(name, help string) m.Gauge {
+	return m.NewGauge("", "http_requests", name, "http_requests "+help)
 }
-func newCounterIncomingTraffic(name, help string) prometheus.Counter {
-	return m.NewCounter("incoming_"+name, "incoming "+help)
+func neGaugeIncomingTraffic(name, help string) m.Gauge {
+	return m.NewGauge("", "incoming", name, "incoming "+help)
 }
-func newCounterContentd(name, help string) prometheus.Counter {
-	return m.NewCounter("contentd_"+name, "contentd "+help)
+func newGaugeContentd(name, help string) m.Gauge {
+	return m.NewGauge("", "contentd", name, "contentd "+help)
 }
 
 func Init(appName string) {
 
 	m.Init(appName)
-	Overall = newCounterHttpRequests("overall", "overall")
-	Access = newCounterHttpRequests("access", "opened static")
-	Agree = newCounterHttpRequests("agreed", "pressed the button 'agree'")
-	AgreeSuccess = newCounterHttpRequests("agree_success", "pressed the button 'agree' and successfully processed")
-	Errors = newCounterHttpRequests("error", "error")
-	CampaignHashWrong = newCounterHttpRequests("campaign_hash_wrong", "campaign hash wrong")
+	Overall = newGaugeHttpRequests("overall", "overall")
+	Access = newGaugeHttpRequests("access", "opened static")
+	Agree = newGaugeHttpRequests("agreed", "pressed the button 'agree'")
+	AgreeSuccess = newGaugeHttpRequests("agree_success", "pressed the button 'agree' and successfully processed")
+	Errors = m.NewGauge("", "", "errors", "http_requests errors")
+	CampaignHashWrong = newGaugeHttpRequests("campaign_hash_wrong", "campaign hash wrong")
 
-	IPNotFoundError = newCounterIncomingTraffic("ip_not_found", "ip not found")
-	MsisdnNotFoundError = newCounterIncomingTraffic("msisdn_not_found", "msisdn not found")
-	NotSupported = newCounterIncomingTraffic("not_supported", " operator is not supported")
-	GetInfoByMsisdn = newCounterIncomingTraffic("info_by_msisdn", "cannot find info by msisdn")
+	IPNotFoundError = neGaugeIncomingTraffic("ip_not_found", "ip not found")
+	MsisdnNotFoundError = neGaugeIncomingTraffic("msisdn_not_found", "msisdn not found")
+	NotSupported = neGaugeIncomingTraffic("not_supported", " operator is not supported")
+	GetInfoByMsisdn = neGaugeIncomingTraffic("info_by_msisdn", "cannot find info by msisdn")
 
-	ContentDeliveryErrors = newCounterHttpRequests("serve_errors", "content delivery errors")
-	PageNotFoundError = newCounterHttpRequests("error404", "404 requests")
-	ContentdRPCDialError = newCounterContentd("connect_errors", "number of connect errors ")
+	ContentDeliveryErrors = newGaugeHttpRequests("serve_errors", "content delivery errors")
+	PageNotFoundError = newGaugeHttpRequests("error404", "404 requests")
+	ContentdRPCDialError = newGaugeContentd("connect_errors", "number of connect errors ")
 	LoadCampaignError = m.PrometheusGauge(
 		"",
 		"campaign",
 		"load_error",
 		"Load campaign HTML error",
 	)
+
+	go func() {
+		for range time.Tick(time.Minute) {
+			Overall.Update()
+			Access.Update()
+			Agree.Update()
+			AgreeSuccess.Update()
+			Errors.Update()
+			PageNotFoundError.Update()
+			IPNotFoundError.Update()
+			MsisdnNotFoundError.Update()
+			GetInfoByMsisdn.Update()
+			NotSupported.Update()
+			CampaignHashWrong.Update()
+			ContentDeliveryErrors.Update()
+			ContentdRPCDialError.Update()
+		}
+	}()
 }
